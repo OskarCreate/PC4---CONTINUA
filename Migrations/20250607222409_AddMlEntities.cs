2f5f179f17d614c13473ff387d739b3cb68d67fe
OskarCreate <oskar_morales_f@outlook.com>
2025-06-07 17:25:20 -0500

    Migración de la sentiment

---
+++ Migrations/20250607222409_AddMlEntities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PC4___CONTINUA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMlEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SentimentRecords",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Prediction = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Probability = table.Column<float>(type: "REAL", nullable: false),
+                    Score = table.Column<float>(type: "REAL", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SentimentRecords", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SentimentRecords");
+        }
+    }
+}
