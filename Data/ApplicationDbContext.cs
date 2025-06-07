3103cea2e57ba73ad90ab4e2e18e4799efdf1f88
OskarCreate <oskar_morales_f@outlook.com>
2025-06-07 17:14:05 -0500

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PC4___CONTINUA.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
